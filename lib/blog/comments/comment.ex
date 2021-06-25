849ca97a2cbed173d19235d7e2abd5349f7fb027
carlosviana <viana.mail@gmail.com>
2021-06-25 14:46:48 -0300

    temporario

---
+++ lib/blog/comments/comment.ex
@@ -0,0 +1,17 @@
+defmodule Blog.Comments.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "comments" do
+    field :content, :string
+    belongs_to :posts, Blog.Posts.Post
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:content])
+    |> validate_required([:content])
+  end
+end
