1c9314f9ebd1eedf044217549e640ad9ffc97695
carlosviana <viana.mail@gmail.com>
2021-07-02 05:06:17 -0300

    ajustes pipeline

--- lib/blog_web/controllers/plug/require_auth.ex
+++ lib/blog_web/controllers/plug/require_auth.ex
@@ -6,13 +6,13 @@ defmodule BlogWeb.Plug.RequireAuth do
   def init(_), do: nil
 
   def call(conn, _) do
-      if conn.assigns[:user] do
-        conn
-      else
-        conn
-        |> put_flash(:error, "Você precisa estar logado!")
-        |> redirect(to: Routes.page_path(conn, :index))
-        |> halt()
-      end
+    if conn.assigns[:user] do
+      conn
+    else
+      conn
+      |> put_flash(:error, "Você precisa estar logado!")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
+    end
   end
 end
