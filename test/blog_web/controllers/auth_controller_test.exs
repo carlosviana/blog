fbfc6c0ac4af85a31ab5acb29722c9e229c68389
carlosviana <viana.mail@gmail.com>
2021-07-02 21:55:53 -0300

    auth test

--- test/blog_web/controllers/auth_controller_test.exs
+++ test/blog_web/controllers/auth_controller_test.exs
@@ -14,6 +14,19 @@ defmodule BlogWeb.AuthControllerTest do
     provider: "google"
   }
 
+  @ueberauth_error %Ueberauth.Auth{
+    credentials: %{
+      token: nil
+    },
+    info: %{
+      email: "teste@teste.com",
+      first_name: nil,
+      last_name: nil,
+      image: nil
+    },
+    provider: nil
+  }
+
   test "callback sucess", %{conn: conn} do
     conn =
       conn
@@ -25,5 +38,27 @@ defmodule BlogWeb.AuthControllerTest do
       conn = get(conn, Routes.page_path(conn, :index))
       assert html_response(conn, 200) =~ "Bem vindo!!! #{@ueberauth.info.email}"
 
+  end
+
+  test "callback error", %{conn: conn} do
+    conn =
+      conn
+      |> assign(:ueberauth_auth, @ueberauth_error)
+      |> get(Routes.auth_path(conn, :callback, "google"))
+
+      assert redirected_to(conn) == Routes.page_path(conn, :index)
+
+      conn = get(conn, Routes.page_path(conn, :index))
+      assert html_response(conn, 200) =~ "Algo deu errado"
+
+  end
+
+  test "logout sucess", %{conn: conn} do
+    conn =
+      conn
+      |> Plug.Test.init_test_session(user_id: 2)
+      |> get(Routes.auth_path(conn, :logout))
+
+      assert redirected_to(conn) == Routes.page_path(conn, :index)
   end
 end
